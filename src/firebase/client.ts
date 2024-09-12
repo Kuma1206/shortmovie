211ebdef9406f91d88352e251e24a23d752b3498
Kuma1206 <tadakuma369@gmail.com>
2024-09-12 09:19:31 +0900

    Update client.ts

--- src/firebase/client.ts
+++ src/firebase/client.ts
@@ -5,12 +5,7 @@ import { getAuth } from 'firebase/auth';
 import { getFunctions } from 'firebase/functions';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyC12os0qCgyZXVuQrJBZtiwJ-bBFGo94rE",
-  authDomain: "osmproject-34e1b.firebaseapp.com",
-  projectId: "osmproject-34e1b",
-  storageBucket: "osmproject-34e1b.appspot.com",
-  messagingSenderId: "668370771950",
-  appId: "1:668370771950:web:a6b1652219e36aeb9078ff"
+
 };
 
 if (getApps().length === 0) {
