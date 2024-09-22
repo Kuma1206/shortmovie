57b56f64fca79d8b66fab18f55373fa27605c699
Kuma1206 <tadakuma369@gmail.com>
2024-09-22 23:09:06 +0900

    Delete src directory

--- src/types/recordrtc.d.ts
+++
@@ -1,9 +0,0 @@
-declare module 'recordrtc' {
-  export default class RecordRTC {
-    constructor(stream: MediaStream, options?: any);
-    startRecording(): void;
-    stopRecording(callback: (audioURL: string) => void): void;
-    getBlob(): Blob;
-    // 他の必要なメソッドやプロパティをここに追加
-  }
-}
