993bfbfb76c29846ac1ee3181765ae02b821b633
Kuma1206 <tadakuma369@gmail.com>
2024-09-23 00:08:51 +0900

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,1 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  images: {
-    domains: ["firebasestorage.googleapis.com"], // Firebaseの外部ホスト名を追加
-  },
-};
 
-export default nextConfig;
